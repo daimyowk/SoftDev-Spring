1a786a550a997919116dd0c0e8b8053c1d85d686
Javis <wujavis@gmail.com>
2016-04-11 11:55:41 -0400

    added quicksort

--- hw09/hw09.py
+++ hw09/hw09.py
@@ -48,3 +48,28 @@ def hello():
 
 helloHTML=hello()
 print helloHTML
+
+#QUICKSORT
+def quicksort(case):
+    if len(case)<=1:
+        return case
+    pivot=case[len(case)/2]
+    lower=[]
+    upper=[]
+    hold= [1 
+           if y==pivot  
+           else 2 if y>pivot
+           else 0 if y<pivot
+           else 3 
+           for y in case
+           ]
+    upper=[x for x in case
+           if x>=pivot]
+    lower=[x for x in case
+           if x<pivot]
+    upper.remove(pivot)
+    return quicksort(lower)+[pivot]+quicksort(upper)
+
+print quicksort([1,4,2])
+print quicksort([1,151,123,32])
+print quicksort([11,11,11,32,11])
